30ad4ef6d42a5ad0b5723d6eb04998a24f0a291d
enesaydogan <enesaydogan@hotmail.com>
2025-01-07 18:26:28 +0300

    Add files via upload

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-test1",
-      "name": "test1",
+      "id": "0-orta-orta",
+      "name": "ORTA ORTA",
       "levels": [
         {
           "tileSize": 256,
@@ -20,19 +20,340 @@ var APP_DATA = {
       ],
       "faceSize": 1024,
       "initialViewParameters": {
-        "yaw": -0.509581702654355,
-        "pitch": -0.31651124317696677,
+        "yaw": 0.005332357407191068,
+        "pitch": 0.21657956868457617,
         "fov": 1.7951537489170692
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -0.01264706598328047,
+          "pitch": 0.10561945345368962,
+          "rotation": 0,
+          "target": "4-gr-st"
+        },
+        {
+          "yaw": -1.3707411696266014,
+          "pitch": 0.02906574884134372,
+          "rotation": 0,
+          "target": "1-sa-st"
+        },
+        {
+          "yaw": -1.3364486369361686,
+          "pitch": 0.45089894553580123,
+          "rotation": 0,
+          "target": "2-sa-alt"
+        },
+        {
+          "yaw": -0.04613690649874158,
+          "pitch": -1.4197024447157212,
+          "rotation": 0,
+          "target": "5-orta-kubbe"
+        },
+        {
+          "yaw": -0.039297079609807994,
+          "pitch": 0.49895868424506595,
+          "rotation": 0,
+          "target": "3-gr-zemn"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-sa-st",
+      "name": "SAĞ ÜST",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.809166620030065,
+          "pitch": -0.09253424638658458,
+          "rotation": 0,
+          "target": "0-orta-orta"
+        },
+        {
+          "yaw": 1.060007698190983,
+          "pitch": 0.03583505391758379,
+          "rotation": 0,
+          "target": "4-gr-st"
+        },
+        {
+          "yaw": 1.1591096158944545,
+          "pitch": 0.42595743000629227,
+          "rotation": 0,
+          "target": "3-gr-zemn"
+        },
+        {
+          "yaw": 1.8752723640989926,
+          "pitch": -0.5598878431218051,
+          "rotation": 0,
+          "target": "5-orta-kubbe"
+        },
+        {
+          "yaw": 2.1830830858030037,
+          "pitch": 1.50594829309484,
+          "rotation": 0,
+          "target": "2-sa-alt"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-sa-alt",
+      "name": "SAĞ ALT",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 2.1180313753425546,
+        "pitch": -0.20070427860861173,
+        "fov": 1.7951537489170692
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0091439555467776,
+          "pitch": 0.027310128517628485,
+          "rotation": 0,
+          "target": "3-gr-zemn"
+        },
+        {
+          "yaw": 0.9332064891949301,
+          "pitch": -0.6729685777687262,
+          "rotation": 0,
+          "target": "4-gr-st"
+        },
+        {
+          "yaw": 1.8280896847192558,
+          "pitch": -0.3844588327216307,
+          "rotation": 0,
+          "target": "0-orta-orta"
+        },
+        {
+          "yaw": 1.8214435168402368,
+          "pitch": -0.8855492234619788,
+          "rotation": 0,
+          "target": "5-orta-kubbe"
+        },
+        {
+          "yaw": 1.8370887133481508,
+          "pitch": -1.5707963267948966,
+          "rotation": 0,
+          "target": "1-sa-st"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-gr-zemn",
+      "name": "GİRİŞ ZEMİN",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 3.132422581950049,
+        "pitch": -0.15596312501706322,
+        "fov": 1.7951537489170692
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1412402313735406,
+          "pitch": -0.4048408536576016,
+          "rotation": 0,
+          "target": "0-orta-orta"
+        },
+        {
+          "yaw": 3.1362418914823813,
+          "pitch": -0.8894180973883046,
+          "rotation": 0,
+          "target": "5-orta-kubbe"
+        },
+        {
+          "yaw": -2.2368501543810453,
+          "pitch": 0.0020014654095437123,
+          "rotation": 0,
+          "target": "2-sa-alt"
+        },
+        {
+          "yaw": -2.3454264719314786,
+          "pitch": -0.4387845824106389,
+          "rotation": 0,
+          "target": "1-sa-st"
+        },
+        {
+          "yaw": 3.112406823618419,
+          "pitch": -1.5707963267948966,
+          "rotation": 0,
+          "target": "4-gr-st"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-gr-st",
+      "name": "GİRİŞ ÜST",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 3.0713449212167108,
+        "pitch": 0.12657895702450972,
+        "fov": 1.7951537489170692
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.111352453982853,
+          "pitch": 1.5153436828517703,
+          "rotation": 0,
+          "target": "3-gr-zemn"
+        },
+        {
+          "yaw": 3.104631471748343,
+          "pitch": 0.08503392867933357,
+          "rotation": 0,
+          "target": "0-orta-orta"
+        },
+        {
+          "yaw": -3.121161436472182,
+          "pitch": -0.57138217368502,
+          "rotation": 0,
+          "target": "5-orta-kubbe"
+        },
+        {
+          "yaw": -2.3546644464057067,
+          "pitch": 0.38404784417368987,
+          "rotation": 0,
+          "target": "2-sa-alt"
+        },
+        {
+          "yaw": -2.325116169805522,
+          "pitch": -0.0903968891291651,
+          "rotation": 0,
+          "target": "4-gr-st"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-orta-kubbe",
+      "name": "ORTA KUBBE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.02148834001180333,
+        "pitch": 0.2814895383791942,
+        "fov": 1.7951537489170692
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.053530472269507356,
+          "pitch": 0.6016532521658107,
+          "rotation": 0,
+          "target": "4-gr-st"
+        },
+        {
+          "yaw": 0.056844173663016306,
+          "pitch": 0.8457081285940973,
+          "rotation": 0,
+          "target": "3-gr-zemn"
+        },
+        {
+          "yaw": -1.283375720103992,
+          "pitch": 0.48457980616180407,
+          "rotation": 0,
+          "target": "1-sa-st"
+        },
+        {
+          "yaw": -1.2640820748079555,
+          "pitch": 0.7715341176717967,
+          "rotation": 0,
+          "target": "2-sa-alt"
+        },
+        {
+          "yaw": -1.4812323365712068,
+          "pitch": 1.5707963267948966,
+          "rotation": 0,
+          "target": "0-orta-orta"
+        }
+      ],
       "infoHotspots": []
     }
   ],
   "name": "Project Title",
   "settings": {
     "mouseViewMode": "drag",
-    "autorotateEnabled": true,
-    "fullscreenButton": false,
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
     "viewControlButtons": false
   }
 };
